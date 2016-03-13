ff7f5a0a472c5e02e2f877948fae520a4e945f8d
Garincha Utama <garinchutama@gmail.com>
2016-03-13 11:44:07 +0700

    Update Perusahaan.java

--- TUBES Kelompok12 IF-38-10/src/Perusahaan.java
+++ TUBES Kelompok12 IF-38-10/src/Perusahaan.java
@@ -10,5 +10,39 @@
  */
 public class Perusahaan extends Orang{
     private Lowongan[] daftarLowongan;
+    private long id_perusahaan;
+    private int jumlahLowongan;
     
-}
+    public Perusahaan(String nama, String alamat, long id_perusahaan){
+        this.nama = nama;
+        this.alamat = alamat;
+        this.id_perusahaan = id_perusahaan;   
+    }
+
+    public void setId_perusahaan(long id_perusahaan) {
+        this.id_perusahaan = id_perusahaan;
+    }
+
+    public long getId_perusahaan() {
+        return id_perusahaan;
+    }
+    
+    public void createLowongan(){ 
+        daftarLowongan[jumlahLowongan] = new Lowongan(); 
+        jumlahLowongan++; 
+    } 
+    
+    public void removeLowongan(){
+        
+    }
+     
+    public Lowongan getLowonganIndex(int index){ 
+        return daftarLowongan[index];
+    }
+    
+    public Lowongan getLowonganId(int id){ 
+    }
+    
+    } 
+    
+    
