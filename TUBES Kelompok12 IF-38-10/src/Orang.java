2324e1dc3222f97d16df705ac6fc4a9db6a3cffa
Garincha Utama <garinchutama@gmail.com>
2016-03-13 11:47:29 +0700

    Update Orang.java

--- TUBES Kelompok12 IF-38-10/src/Orang.java
+++ TUBES Kelompok12 IF-38-10/src/Orang.java
@@ -8,54 +8,29 @@
  *
  * @author ihsan
  */
+
 public abstract class Orang {
     private String nama;
     private String alamat;
-    private String ktp;
-    private int umur;
     
-    
-    public Orang (String nama) {
+    public void setNama(String nama) {
         this.nama = nama;
-    } 
-    
-    public Orang (String nama, String alamat, String ktp, int umur){
-        this.nama = nama;
+    }
+
+    public void setAlamat(String alamat) {
         this.alamat = alamat;
-        this.ktp = ktp;
-        this.umur = umur;
     }
-    
-    public void setNama (String nama){
-        this.nama = nama;
-    }
-    
-    public String getNama (){
+
+
+    public String getNama() {
         return nama;
     }
-    
-    public void setAlamat (String alamat){
-        this.alamat = alamat;
-    }
-    
-    public String getAlamat (){
+
+    public String getAlamat() {
         return alamat;
     }
-    
-    public void setKtp (String ktp){
-        this.ktp = ktp;
-    }
-    
-    public String getKtp (){
-        return ktp;
-    }
-    
-    public void setUmur (int umur){
-        this.umur = umur;
-    }
-    
-    public int getUmur (){
-        return umur;
-    }
-    
+ 
 }
+    
+  
+
