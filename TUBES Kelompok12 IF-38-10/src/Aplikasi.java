b781e1029e5ebfd057768c94bbd28f9c9693d637
Garincha Utama <garinchutama@gmail.com>
2016-03-30 23:18:11 +0700

    Added files via upload

---
+++ TUBES Kelompok12 IF-38-10/src/Aplikasi.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Garincha
+ */
+public class Aplikasi {
+    private Perusahaan[] daftarPerusahaan;
+    private Pelamar[] daftarPelamar;
+}
+//Perusahaan dan pelamar dapat dijadikan 1 array Orang
+// tidak diperlukan array/list Lowongan dan BerkasLamaran karena berada di dalam Perusahaan dan pelamar
+
+public void addPerusahaan(String nama, String alamat, long id_perusahaan){
+    
+}
+
+public void addPelamar(String nama, String alamat, long id_ktp){
+
+}
+
+public Pelamar getPelamar(id){
+
+}
+
+public void deletePelamar(id){
+
+}
+
+
+
